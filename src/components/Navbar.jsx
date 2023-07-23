64f7239b1dee3345d3a1f976349245fc3896c798
Mohammed <muaminab@gmail.com>
2023-07-23 17:40:23 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { BsXLg } from "react-icons/bs";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <div className="navbar ">
-      <div className="nav">
-        <a href="#aboutus">About Us </a>
-        <a href="#tours">Tours</a>
-        <a href="#entertainment">Entertainment </a>
-      </div>
-      <div className="nav-island-wonders">Island Wonders</div>
+  const [togglemenu, setTogglemenu] = useState(false);
 
-      <div className="nav">
-        <a href="#promo">Promo</a>
-        <a href="#reviews">Reviews</a>
-        <a href="#contacts">Our Contacts </a>{" "}
+  return (
+    <>
+      <div className="navbar ">
+        <div className="nav">
+          <a href="#aboutus">About Us </a>
+          <a href="#tours">Tours</a>
+          <a href="#entertainment">Entertainment </a>
+        </div>
+        <div className="nav-island-wonders">Island Wonders</div>
+        <div className="nav">
+          <a href="#promo">Promo</a>
+          <a href="#reviews">Reviews</a>
+          <a href="#contacts">Our Contacts </a>
+        </div>
+        <div className="bar" onClick={() => setTogglemenu(true)}>
+          <FaBars />
+        </div>
       </div>
-    </div>
+      {togglemenu && (
+        <div className="smallscreen-nav">
+          <div className="closed-menubar" onClick={() => setTogglemenu(false)}>
+            <BsXLg />
+          </div>
+          <div className="smallscreen-navlinks">
+            <a href="#aboutus" onClick={() => setTogglemenu(false)}>
+              About Us
+            </a>
+            <a href="#tours" onClick={() => setTogglemenu(false)}>
+              Tours
+            </a>
+            <a href="#entertainment" onClick={() => setTogglemenu(false)}>
+              Entertainment
+            </a>
+            <a href="#promo" onClick={() => setTogglemenu(false)}>
+              Promo
+            </a>
+            <a href="#reviews" onClick={() => setTogglemenu(false)}>
+              Reviews
+            </a>
+            <a href="#contacts" onClick={() => setTogglemenu(false)}>
+              Our Contacts
+            </a>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
